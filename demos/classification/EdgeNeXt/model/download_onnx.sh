21452e18f32fa4d8d25e3b28947d0291f7db99ec
Bartonzany <LinXi198913@163.com>
2024-09-05 11:22:42 +0800

    modified README file

--- demos/classification/EdgeNeXt/model/download_onnx.sh
+++ demos/classification/EdgeNeXt/model/download_onnx.sh
@@ -1,5 +1,6 @@
 # Download onnx models weights
 
-wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/EdgeNeXt_base_224x224_nv12.bin
-wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/EdgeNeXt_small_224x224_nv12.bin
-wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/EdgeNeXt_x_small_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/edgenext_base.onnx
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/edgenext_small.onnx
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/edgenext_x_small.onnx
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/edgenext_xx_small.onnx
