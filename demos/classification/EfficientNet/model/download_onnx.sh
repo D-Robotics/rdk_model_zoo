af436cf98ed62bd85405ac91cea2b577f562eea1
Bartonzany <LinXi198913@163.com>
2024-09-06 13:31:26 +0800

    add EfficientNet model file

---
+++ demos/classification/EfficientNet/model/download_onnx.sh
@@ -0,0 +1,5 @@
+# Download onnx models weights
+
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/efficientnet_b2.onnx
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/efficientnet_b3.onnx
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/efficientnet_b4.onnx
