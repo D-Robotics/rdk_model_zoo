a404c7bbf7600ce1971f1f23d3486ea99f603b4b
Bartonzany <LinXi198913@163.com>
2024-09-09 17:54:28 +0800

    add RepViT model files

---
+++ demos/classification/RepViT/model/download.sh
@@ -0,0 +1,5 @@
+# Download bin models weights
+
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepViT_m0_9_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepViT_m1_0_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepViT_m1_1_224x224_nv12.bin
