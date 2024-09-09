c71e381a66c61ab3931e5248ef849ede80fa5d73
Bartonzany <LinXi198913@163.com>
2024-09-09 17:53:22 +0800

    add RepGhost model files

---
+++ demos/classification/RepGhost/model/download.sh
@@ -0,0 +1,7 @@
+# Download bin models weights
+
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepGhost_100_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepGhost_111_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepGhost_130_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepGhost_150_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/RepGhost_200_224x224_nv12.bin
