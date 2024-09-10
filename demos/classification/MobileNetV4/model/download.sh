04fc87517ab0bfad2b69d0715c0da1308b6895e4
Bartonzany <LinXi198913@163.com>
2024-09-10 17:14:38 +0800

    add mobilenetv4 model files

--- demos/classification/MobileNetV4/model/download.sh
+++ demos/classification/MobileNetV4/model/download.sh
@@ -1,3 +1,3 @@
 # Download bin models weights
 
-wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x5/MobileNetV4_conv_small_224x224_nv12.bin
+wget https://archive.d-robotics.cc/downloads/rdk_model_zoo/rdk_x3/MobileNetV4_224x224_nv12.bin
